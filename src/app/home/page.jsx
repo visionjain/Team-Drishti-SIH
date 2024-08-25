5c7ef30bb5cb0b90849478bedc5b3dc290b052d5
Vision Jain <visionjain118@gmail.com>
2024-08-25 20:13:43 +0530

    intial

---
+++ src/app/home/page.jsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/hero/hero'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className='bg-red-500'>this is the home page</div>
+            <div><Hero /></div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className='bg-yellow-200'>this is hero section</div>
+    </div>
+  )
+}
+
+export default Hero
