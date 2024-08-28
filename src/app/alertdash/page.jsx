c6408d1b0f0924fde616d65c607dc12e2e3a6cfe
Vision Jain <visionjain118@gmail.com>
2024-08-29 02:35:56 +0530

    sidebar

--- src/app/alertdash/page.jsx
+++ src/app/alertdash/page.jsx
@@ -1,9 +1,8 @@
 "use client";
-import CamTable from '@/components/CamTable/page';
 import Image from 'next/image';
 import React from 'react';
 import { FaCameraRetro } from 'react-icons/fa';
-import { Tooltip, IconButton } from '@material-tailwind/react';
+import { Tooltip, Card, IconButton } from '@material-tailwind/react';
 import { useRouter } from 'next/navigation';
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { AiFillAlert } from "react-icons/ai";
@@ -20,12 +19,12 @@ const Home = () => {
         router.push('/alertdash'); // Navigate to /camera
     };
 
-    return (
-        <div className="flex flex-col h-screen w-screen">
-            <div className="flex flex-1">
-                <div className="w-26 bg-[#424242] p-2 pb-1">
-                    <div className="bg-white h-full rounded-xl flex flex-col items-center">
-                        <Tooltip content="SIH'24" placement="right">
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <div className="flex flex-1">
+        <div className="w-26 bg-[#424242] p-2 pb-1">
+          <div className="bg-white h-full rounded-xl flex flex-col items-center">
+          <Tooltip content="SIH'24" placement="right">
                             <div className="relative border-2 border-black w-12 h-12 m-1 rounded-xl cursor-pointer">
                                 <a href="https://www.sih.gov.in/" target="_blank" rel="noopener noreferrer">
                                     <Image
@@ -82,19 +81,20 @@ const Home = () => {
                                 <AiFillAlert size={30} /> {/* Increase icon size */}
                             </IconButton>
                         </Tooltip>
-                    </div>
-                </div>
-                <div className="flex-1 bg-[#424242] h-full pt-2 pl-2 pr-2">
-                    <CamTable />
-                </div>
-            </div>
-            <div className="bg-gray-800 w-full text-center py-2">
-                <div className='bg-white mb- ml-2 mr-2 p-1 rounded-xl'>
-                    &#169; Team DRISHTI - SIH
-                </div>
-            </div>
+          </div>
         </div>
-    );
+        <div className="flex-1 bg-[#424242] h-full pt-2 pl-2 pr-2">
+          <Card className="h-full w-full">
+          </Card>
+        </div>
+      </div>
+      <div className="bg-gray-800 w-full text-center py-2">
+        <div className=' bg-white mb- ml-2 mr-2 p-1 rounded-xl'>
+          &#169; Team DRISHTI - SIH
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
